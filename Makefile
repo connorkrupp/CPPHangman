54991a84e2210f1850898879cc2f718e02369542
Connor Krupp <connorkrupp@icloud.com>
2015-09-15 21:15:45 -0400

    Now displays current progress and the Makefile is more in depth

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
-app : main.cpp
-			g++ -Wall -Werror -pedantic -O1 main.cpp -o app
-			./app
-			rm -vf app *~ *.out
+app : main.cpp compile
+			./Hangman
+			rm -vf Hangman *~ *.out
+
+build : main.cpp compile
+
+compile : main.cpp
+					g++ -Wall -Werror -pedantic -O1 main.cpp -o Hangman
+
+clean :
+					rm -vf Hangman *~ *.out
--- main.cpp
+++ main.cpp
@@ -4,56 +4,43 @@
 #include <iostream>
 
 std::string word;
-std::string currentGuess;
+std::string wordInProgress;
 
-void buildCurrentGuess();
+void buildWordInProgress();
 bool wordSolved();
 bool addLetter(char letter);
+bool startGame();
 
 int main(int argc, char const *argv[]) {
-
 
   std::cout << "User 1 enter a word: ";
   std::cin >> word;
 
-  buildCurrentGuess();
+  buildWordInProgress();
 
-  int guessesRemaining = 5;
-  while (!wordSolved()) {
-    char letter;
-    std::cout << "Guess a Letter: ";
-    std::cin >> letter;
-
-    if (!addLetter(letter)) {
-      guessesRemaining--;
-      std::cout << "Wrong! " << guessesRemaining << " guesses remaining..." << std::endl;
-    }
-
-    if (guessesRemaining == 0) {
-      std::cout << "You Suck Ass!" << std::endl;
-      return 0;
-    }
+  if (startGame()) {
+    std::cout << word << " Solved!" << std::endl;
+  } else {
+    std::cout << "You Suck Ass!" << std::endl;
   }
-
-  std::cout << "Word Solved!" << std::endl;
 
   return 0;
 }
 
-// Modifies: currentGuess
+// Modifies: wordInProgress
 // Effects: creates underscore+space version of word
-void buildCurrentGuess() {
+void buildWordInProgress() {
   for (size_t i = 0; i < word.size(); i++) {
-    currentGuess.push_back('_');
-    if (i != word.size() - 1) { currentGuess.push_back(' '); }
+    wordInProgress.push_back('_');
+    if (i != word.size() - 1) { wordInProgress.push_back(' '); }
   }
 }
 
-// Effects: returns whether currentGuess is equivalent to the word
+// Effects: returns whether wordInProgress is equivalent to the word
 bool wordSolved() {
   std::string guessedWord;
-  for (size_t i = 0; i < currentGuess.size(); i++) {
-    char currentChar = currentGuess.at(i);
+  for (size_t i = 0; i < wordInProgress.size(); i++) {
+    char currentChar = wordInProgress.at(i);
     if (currentChar != ' ' && currentChar != '_') {
       guessedWord.push_back(currentChar);
     }
@@ -65,14 +52,37 @@ bool wordSolved() {
   return false;
 }
 
-// Modifies: currentGuess
+// Modifies: wordInProgress
 // Effects: returns false if letter is not found, otherwise writes letter to
-// currentGuess
+//          wordInProgress
 bool addLetter(char letter) {
   std::size_t index = word.find(letter);
   if (index != std::string::npos) {
-    currentGuess.at((index+1) * 2 - 2) = letter;
+    wordInProgress.at((index+1) * 2 - 2) = letter;
     return true;
   }
   return false;
+}
+
+// Effects: returns whether the game was won or lost
+bool startGame() {
+
+  int guessesRemaining = 5;
+  while (!wordSolved()) {
+    char letter;
+    std::cout << wordInProgress << std::endl;
+    std::cout << "Guess a Letter: ";
+    std::cin >> letter;
+
+    if (!addLetter(letter)) {
+      guessesRemaining--;
+      std::cout << "Wrong! " << guessesRemaining << " guesses remaining..." << std::endl;
+    }
+
+    if (guessesRemaining == 0) {
+      return false;
+    }
+  }
+  return true;
+
 }
